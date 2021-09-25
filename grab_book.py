197437165dbcf7b4d0808624f66257440c48e31d
Joseph Chiocchi <joe@yolk.cc>
2021-09-25 14:37:23 -0500

    organizing data

--- grab_book.py
+++ grab_book.py
@@ -1,7 +1,8 @@
 """
 Grab book offers
 """
-from glom import glom
+from glom import glom, Coalesce, SKIP
+from rich import print
 from xrpl.clients.json_rpc_client import JsonRpcClient
 from xrpl.models.currencies import IssuedCurrency, XRP
 from xrpl.models.requests.book_offers import BookOffers
@@ -11,7 +12,8 @@ MAINNET_BITSTAMP = "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B"
 MAINNET_GATEHUB = "rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq"
 
 
-client = JsonRpcClient("https://xrplcluster.com")
+# client = JsonRpcClient("https://xrplcluster.com")
+client = JsonRpcClient("http://localhost:5005")
 
 
 EUR_bitstamp = IssuedCurrency(currency="EUR", issuer=MAINNET_BITSTAMP)
@@ -55,7 +57,15 @@ bitstamp_xrp__usd_offers = get_book_offers(USD_bitstamp, XRP())
 gatehub_usd__xrp_offers = get_book_offers(XRP(), USD_gatehub)
 gatehub_xrp__usd_offers = get_book_offers(USD_gatehub, XRP())
 
+gatehub_eur_bitstamp_eur_offers = get_book_offers(EUR_gatehub, EUR_bitstamp)
 
+bitstamp_usd_gatehub_eur_offers = get_book_offers(USD_bitstamp, EUR_gatehub)
+bitstamp_usd_eur_offers = get_book_offers(USD_bitstamp, EUR_bitstamp)
+gatehub_usd_eur_offers = get_book_offers(USD_gatehub, EUR_gatehub)
+bitstamp_usd_bitstamp_jpy_offers = get_book_offers(USD_bitstamp, JPY_bitstamp)
+gatehub_usd_bitstamp_jpy_offers = get_book_offers(USD_gatehub, JPY_bitstamp)
+
+# 'any' refers to 'any issued currency'; xrp is the native asset.
 glom_spec_xrp_any = {
     "TakerGets": "TakerGets",
     "TakerPays": ("TakerPays", "value"),
@@ -65,6 +75,16 @@ glom_spec_any_xrp = {
     "TakerPays": "TakerPays",
     "TakerGets": ("TakerGets", "value"),
 }
+
+glom_spec_any_any = {
+    "TakerGets": ("TakerGets", "value"),
+    "TakerPays": ("TakerPays", "value"),
+}
+
+glom_spec_account_only = {
+    "Account": "Account"
+}
+
 # combined_usd_offers = glom(bitstamp_usd__xrp_offers, [glom_spec_xrp_any]) + glom(
 #     gatehub_usd__xrp_offers, [glom_spec_xrp_any]
 # )
@@ -78,29 +98,51 @@ combined_usd_offers = [
         ]
     ]
     + [
-        glom(offers, [glom_spec_any_xrp])
+        glom(offers, [Coalesce(glom_spec_any_any, SKIP)])
         for offers in [
-            bitstamp_xrp__usd_offers,
-            gatehub_xrp__usd_offers,
+            bitstamp_usd_gatehub_eur_offers,
+            bitstamp_usd_eur_offers,
+            gatehub_usd_eur_offers,
+            bitstamp_usd_bitstamp_jpy_offers,
+            gatehub_usd_bitstamp_jpy_offers,
         ]
     ]
     for offer in glommed
 ]
 
-combind_eur_offers = [
-    offer
+all_accounts = [
+    account
     for glommed in [
-        glom(offers, [glom_spec_xrp_any])
+        glom(offers, [glom_spec_account_only])
         for offers in [
-            get_book_offers(XRP(), EUR_bitstamp),
-            get_book_offers(XRP(), EUR_gatehub),
+            bitstamp_usd__xrp_offers,
+            gatehub_usd__xrp_offers,
+            bitstamp_usd_gatehub_eur_offers,
+            bitstamp_usd_eur_offers,
+            gatehub_usd_eur_offers,
+            bitstamp_usd_bitstamp_jpy_offers,
+            gatehub_usd_bitstamp_jpy_offers,
         ]
     ]
     for offer in glommed
+    for account in offer.values()
 ]
-    # ] + [
-    #     glom(offers, [glom_spec_any_xrp])
-    #     for offers in [
-    #         get_book_offers(EUR_bitstamp, 
 
+# combind_eur_offers = [
+#     offer
+#     for glommed in [
+#         glom(offers, [glom_spec_xrp_any])
+#         for offers in [
+#             get_book_offers(XRP(), EUR_bitstamp),
+#             get_book_offers(XRP(), EUR_gatehub),
+#         ]
+#     ]
+#     for offer in glommed
+# ]
+
+
+# print(gatehub_usd_eur_offers[5:8])
 print(len(combined_usd_offers))
+print(combined_usd_offers[5:8])
+print(len(all_accounts))
+print(all_accounts[5:8])

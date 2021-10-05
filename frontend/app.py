ee132ae40f483e6e3ebcd1009a47804a037b1285
Joseph Chiocchi <joe@yolk.cc>
2021-10-05 18:15:15 -0500

    move frontend from gh-pages into lambda w/ jinja2
    for inserting our issuers as a cached items scan

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,35 @@
+import os
 import pathlib
 
+import boto3
 
-index_html = pathlib.Path("index.html").read_text()
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+dynamodb = boto3.resource("dynamodb")
+
+ISSUERS_TABLE_NAME = os.environ["ISSUERS_TABLE_NAME"]
+
+issuers_table = dynamodb.Table(ISSUERS_TABLE_NAME)
+
+# index_html = pathlib.Path("index.html").read_text()
+
+jinja_env = Environment(
+    loader=FileSystemLoader("templates"),
+    autoescape=select_autoescape(),
+)
+
+index_template = jinja_env.get_template("index.html")
+
+issuers_table_scan_resp = issuers_table.scan()
+issuers = issuers_table_scan_resp["Items"]
 
 
 def handler(event, context):
+    print("##EVENT")
+    print(event)
+    print("issuers are", issuers)
+    index_html = index_template.render({})
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "text/html"},

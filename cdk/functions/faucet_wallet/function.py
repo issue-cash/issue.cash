87e8f99826a349c40ab489a44aea244efdf73c31
Joseph Chiocchi <joe@yolk.cc>
2021-10-06 21:51:48 -0500

    cleanup

--- cdk/functions/faucet_wallet/function.py
+++ cdk/functions/faucet_wallet/function.py
@@ -1,10 +1,20 @@
+"""
+Wrapping xrpl-py, a function that's pluggable into a FaaS platform that will
+handle it's timeout and provisioning
+
+If this wasn't the testnet, things wouldn't be so loose. 
+
+i.e., We don't care if we loose the temp_wallet because of a timeout.
+"""
 import time
 import random
+
 from xrpl.clients import JsonRpcClient
 from xrpl.wallet import generate_faucet_wallet
 
 
 faucet_client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
+
 
 def handler(event, context):
     temp_wallet = None
